45577793dbb13c225fe7452a58dd74fda497d968
Max Stemplinger <ms@campudus.com>
2014-09-16 09:47:23 +0200

    date, datetime and timestamp tests

--- src/test/scala/io/vertx/asyncsql/test/mysql/MySqlTest.scala
+++ src/test/scala/io/vertx/asyncsql/test/mysql/MySqlTest.scala
@@ -1,7 +1,9 @@
 package io.vertx.asyncsql.test.mysql
 
-import org.vertx.scala.core.json.Json
 import io.vertx.asyncsql.test.{BaseSqlTests, SqlTestVerticle}
+import org.junit.Test
+import org.vertx.scala.core.json._
+import org.vertx.testtools.VertxAssert._
 
 class MySqlTest extends SqlTestVerticle with BaseSqlTests {
 
@@ -14,6 +16,14 @@ class MySqlTest extends SqlTestVerticle with BaseSqlTests {
 
   override def getConfig = config
 
+  override def createDateTable(dateDataType: String) = s"""
+      |  CREATE TABLE date_test (
+      |    id INT NOT NULL AUTO_INCREMENT,
+      |    test_date $dateDataType,
+      |    PRIMARY KEY(id)
+      |  );
+    """.stripMargin
+
   override def createTableStatement(tableName: String) = """
 CREATE TABLE """ + tableName + """ (
   id INT NOT NULL AUTO_INCREMENT,
@@ -24,7 +34,21 @@ CREATE TABLE """ + tableName + """ (
   money FLOAT,
   wedding_date DATE,
   PRIMARY KEY (id)
-);
-                               """
+);"""
+
+  @Test
+  def datetimeTest(): Unit =
+    (for {
+      (m, r) <- sendOk(raw("DROP TABLE IF EXISTS date_test"))
+      (msg, r2) <- sendOk(raw(createDateTable("datetime")))
+      (msg, insertReply) <- sendOk(raw("INSERT INTO date_test (test_date) VALUES ('2015-04-04');"))
+      (msg, reply) <- sendOk(raw("SELECT test_date FROM date_test"))
+    } yield {
+      val receivedFields = reply.getArray("fields")
+      assertEquals(Json.arr("test_date"), receivedFields)
+      logger.info("date is: " + reply.getArray("results").get[JsonArray](0).get[String](0));
+      assertEquals("2015-04-04T00:00:00.000", reply.getArray("results").get[JsonArray](0).get[String](0))
+      testComplete()
+    }) recover failedTest
 
 }
